6cdc0b2288b2fd33de0315c6028d7714a6e3f8bb
Janice Escobar <janice.escobar@aol.com>
2026-06-01 14:33:27 -0400

    added queries

---
+++ 02_product_list.sql
@@ -0,0 +1,17 @@
+use northwind;
+-- What is the name of the table that hold
+-- # 1 What is the name of the table that holds the items Northwind sells?
+
+-- Products
+
+-- #2 Write a query to list the product id, product name, and unit price of every product.
+
+SELECT 
+ProductId as 'Product ID',
+ProductName as 'Product Name',
+UnitPrice as 'Unit Price'
+from Products;
+
+
+
+
